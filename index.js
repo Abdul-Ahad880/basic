d7d8f9b133e7d7f3ced2636c1dcf67bebc9b12db
Abdul-Ahad880 <154658322+Abdul-Ahad880@users.noreply.github.com>
2024-12-10 12:48:18 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+
+app.get("/api", (req, res) => {
+    res.json({
+        message: "Welcome to the Express API!",
+        status: "success",
+        data: { id: 1, name: "Express Example" },
+    });
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
